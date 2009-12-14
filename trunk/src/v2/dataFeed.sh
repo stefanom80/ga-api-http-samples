78c22a2bd9b1e36cf946b0600237f68a10b9ebd9
api.nickm <api.nickm@4f5e8592-246d-11de-8d93-13872bd6126d>
2009-12-14 22:14:50 +0000

    samples for Google Analytics API version 2

---
+++ trunk/src/v2/dataFeed.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Copyright 2009 Google Inc. All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Access the Data Feed of the GA Data Export API through cURL
+
+USER_EMAIL="" #Insert your Google Account email address here
+USER_PASS="" #Insert your password here
+TABLE_ID="" #Insert your table ID here (ie ga:1234)
+
+googleAuth="$(curl https://www.google.com/accounts/ClientLogin -s \
+  -d Email=$USER_EMAIL \
+  -d Passwd=$USER_PASS \
+  -d accountType=GOOGLE \
+  -d source=curl-dataFeed-v2 \
+  -d service=analytics \
+  | awk /Auth=.*/)"
+
+feedUri="https://www.google.com/analytics/feeds/data\
+?ids=$TABLE_ID\
+&start-date=2008-10-01\
+&end-date=2008-10-31\
+&dimensions=ga:source,ga:medium\
+&metrics=ga:visits,ga:bounces\
+&sort=-ga:visits\
+&filters=ga:medium%3D%3Dreferral\
+&max-results=5\
+&prettyprint=true"
+
+curl $feedUri --silent \
+  --header "Authorization: GoogleLogin $googleAuth" \
+  --header "GData-Version: 2"
